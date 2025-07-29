65480bfe823453fcb5975f567898a97d995815c5
Anushacm2 <167397695+Anushacm2@users.noreply.github.com>
2025-07-29 15:14:59 +0530

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,15 @@
+package string;
+
+public class Character {
+	 public static void main(String[] args)
+	 {
+	 String str = "Java Exercises.";
+	 // Convert the above string to a char array.
+	 char[] arr = str.toCharArray();
+	 // Display the contents of the char array.
+	 System.out.println(arr);
+	 }
+	}
+
+
+
