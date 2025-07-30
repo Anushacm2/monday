80bf5ae422b8ac653252da7302f3f5a332cf9dc1
Anushacm2 <167397695+Anushacm2@users.noreply.github.com>
2025-07-30 11:46:10 +0530

    Add files via upload

---
+++ Getpropertiesdemo.java
@@ -0,0 +1,11 @@
+package exceptionerror;
+import java.util.*;
+public class Getpropertiesdemo {
+	
+	public static void main (String []  args) {
+	Properties x = System. getProperties () ;
+	x. list (System. out) ;
+	}
+}
+
+
