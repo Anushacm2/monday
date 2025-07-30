02e7dcfd7f85ba22a7b934f0049f2cd6b3bf20dd
Anushacm2 <167397695+Anushacm2@users.noreply.github.com>
2025-07-30 15:05:02 +0530

    Add files via upload

---
+++ Pathexists.java
@@ -0,0 +1,21 @@
+package javaio;
+import java.io.File;
+
+public class Pathexists {
+	
+	       public static void main(String[] args) {
+	        // Create a File object
+	        File my_file_dir = new File("This PC");
+	         if (my_file_dir.exists()) 
+	           {
+	            System.out.println("The directory or file exists.\n");
+	           } 
+	         else
+	          {
+	            System.out.println("The directory or file does not exist.\n");
+	          }
+	       
+	       }
+}
+
+
