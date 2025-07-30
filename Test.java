8a70eb1451e24ea3f6ad7da664ce41522e444526
Anushacm2 <167397695+Anushacm2@users.noreply.github.com>
2025-07-30 11:46:37 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,29 @@
+package exceptionerror;
+import java.io.File;
+import java.io.FilenameFilter;
+
+public class Test {
+	
+	    public static void main(String[] args) {
+	        File file = new File("C:\\Program Files (x86)\\Lenovo\\LenovoNow\\x86");
+
+	        // Filter files that end with ".py"
+	        String[] fList = file.list(new FilenameFilter() {
+	            @Override
+	            public boolean accept(File dir, String name) {
+	                if (name.toLowerCase().endsWith(".dll")) {
+	                    return true;
+	                } else {
+	                    return false;
+	                }
+	            }
+	        });
+
+	        // Print the filtered file names
+	        for (String f : fList) {
+	            System.out.println(f);
+	        }
+	    }
+	}
+
+
