84da08a6531760ac6796d1224a8b864777302750
Anushacm2 <167397695+Anushacm2@users.noreply.github.com>
2025-07-31 14:53:26 +0530

    Add files via upload

---
+++ Createandprint.java
@@ -0,0 +1,18 @@
+package arraylist;
+import java.util.*;
+
+public class Createandprint {
+	
+	  public static void main(String[] args) {
+	  List<String> list_Strings = new ArrayList<String>();
+	  list_Strings.add("Red");
+	  list_Strings.add("Pink");
+	  list_Strings.add("Purpule");
+	  list_Strings.add("White");
+	  list_Strings.add("Black");
+	  System.out.println(list_Strings);
+	 }
+	}
+
+
+
