d87c4df07b8ea746330d4073143b14d5e687b3c2
Anushacm2 <167397695+Anushacm2@users.noreply.github.com>
2025-08-01 11:53:40 +0530

    Add files via upload

---
+++ Minimumnumberfromlist.java
@@ -0,0 +1,24 @@
+package streamapi;
+import java.util.List;
+
+import java.util.ArrayList;
+
+public class Minimumnumberfromlist {
+	public static void main(String[] args) {
+	List<Integer> al = new ArrayList<Integer>();
+
+	al.add(23);
+
+	al.add(34);
+
+	al.add(67);
+
+	int m =al.stream().min((x, y) -> x > y? 1: -1).get();
+
+	System.out.println(m);
+
+
+	
+
+}
+}
