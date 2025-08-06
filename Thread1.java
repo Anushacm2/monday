092a59fbd6854bbdc1a0299cb9d45d39ca70f242
Anushacm2 <167397695+Anushacm2@users.noreply.github.com>
2025-08-06 19:42:27 +0530

    Add files via upload

---
+++ Thread1.java
@@ -0,0 +1,22 @@
+package assessment;
+
+public class Thread1 {
+	
+	    public static void main(String[] args) {
+	        Thread t1 = new Thread(() -> {
+	            System.out.println("Thread name: " + Thread.currentThread().getName());
+	        });
+
+	        Thread t2 = new Thread(() -> {
+	            System.out.println("Thread name: " + Thread.currentThread().getName());
+	        });
+
+	        t1.setName("Scooby");
+	        t2.setName("Shaggy");
+
+	        t1.start();
+	        t2.start();
+	    }
+	}
+
+
