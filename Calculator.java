5c7e810924c438a9c7d708778462feb7763932b5
Anushacm2 <167397695+Anushacm2@users.noreply.github.com>
2025-08-09 11:21:15 +0530

    Add files via upload

--- Calculator.java
+++ Calculator.java
@@ -1,26 +1,11 @@
-package day3;
+package day1;
 
 public class Calculator {
-
-
-	    // Static method for int power calculation
-	    public static int powerInt(int num1, int num2) {
-	        return (int) Math.pow(num1, num2);
-	    }
-
-	    // Static method for double power calculation
-	    public static double powerDouble(double num1, int num2) {
-	        return Math.pow(num1, num2);
-	    }
-
-	    public static void main(String[] args) {
-	        // Test powerInt
-	        int result1 = Calculator.powerInt(2, 3); // 2^3 = 8
-	        System.out.println("2^3 (int) = " + result1);
-
-	        // Test powerDouble
-	        double result2 = Calculator.powerDouble(2.5, 3); // 2.5^3 = 15.625
-	        System.out.println("2.5^3 (double) = " + result2);
-	    }
+	public int add(int x ,int y) {
+		return x+y;
+	}
+	public int sub(int x ,int y) {
+		return x-y;
 	}
 
+}
